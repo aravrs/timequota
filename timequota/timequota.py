157574d5c145cf73be3774c66f546f4c2fd38e1f
Arav RS <aravindh.ravishankar@gmail.com>
2021-09-14 21:06:52 +0530

    🔨 Refactor Code: Enhancements and fixes

--- timequota/timequota.py
+++ timequota/timequota.py
@@ -7,6 +7,11 @@ from colorama import Fore, Style
 from collections import defaultdict
 from typing import Any, Optional, Iterable, Callable, Union
 
+_time_dict = {
+    "s": 1,
+    "m": 60,
+    "h": 3600,
+}
 
 _color_dict = {
     "g": Fore.GREEN,
@@ -25,28 +30,22 @@ class TimeQuota:
         display_mode: Optional[str] = None,
         *,
         name: Optional[str] = "tq",
-        step_aggr_fn: Optional[Callable] = mean,
+        step_aggr_fn: Optional[Callable[[list[float]], float]] = mean,
         timer_fn: Optional[str] = "perf_counter",
-        logger_fn: Optional[Callable] = print,
+        logger_fn: Optional[Callable[[str], None]] = print,
         precision: Optional[int] = 4,
         color: Optional[bool] = True,
         verbose: Optional[bool] = True,
     ) -> None:
         self.mode = mode.lower()
         self.display_mode = self.mode if display_mode is None else display_mode.lower()
-
-        self.quota = 0
-        if self.mode == "s":
-            self.quota = quota
-        elif self.mode == "m":
-            self.quota = quota * 60
-        elif self.mode == "h":
-            self.quota = quota * 3600
+        self.quota = float(quota) * _time_dict.get(self.mode)
 
         self.name = name
         self.step_aggr_fn = step_aggr_fn
         self.timer_fn = getattr(time, timer_fn)
         self.logger_fn = logger_fn
+
         self.precision = precision
         self.color = _color_dict if color else defaultdict(str)
         self.verbose = verbose
@@ -66,62 +65,69 @@ class TimeQuota:
             self.time_steps.append(self.time_this_step)
             self.time_per_step = self.step_aggr_fn(self.time_steps)
 
-        self.time_exceeded = (self.time_remaining < 0) or (
-            self.time_per_step > self.time_remaining
-        )
+        self.overflow = self.time_remaining < 0
+        self.predicted_overflow = self.time_per_step > self.time_remaining
+
+        self.time_exceeded = self.overflow or self.predicted_overflow
 
     def _get_display_string(
         self,
         seconds: Union[int, float],
     ) -> str:
+
         if self.display_mode == "p":
             return self._get_pretty_string(seconds)
 
-        time_sign = "-" if seconds < 0 else ""
-        seconds = abs(seconds)
+        if seconds == float("inf"):
+            display_string = "inf"
+        else:
+            time_sign = "-" if seconds < 0 else ""
+            seconds = abs(seconds)
 
-        s = seconds
-        m = seconds / 60
-        h = seconds / 3600
+            s = seconds / _time_dict.get("s")
+            m = seconds / _time_dict.get("m")
+            h = seconds / _time_dict.get("h")
 
-        if self.display_mode == "h" and h > 0:
-            return (
-                f"{self.color['c']}{time_sign}{h:.{self.precision}f}h{self.color['R']}"
-            )
-        elif self.display_mode == "m" and m > 0:
-            return (
-                f"{self.color['c']}{time_sign}{m:.{self.precision}f}m{self.color['R']}"
-            )
-        elif self.display_mode == "s" and s > 0:
-            return (
-                f"{self.color['c']}{time_sign}{s:.{self.precision}f}s{self.color['R']}"
-            )
+            if self.display_mode == "h" and h > 0:
+                display_string = f"{time_sign}{h:.{self.precision}f}h"
+            elif self.display_mode == "m" and m > 0:
+                display_string = f"{time_sign}{m:.{self.precision}f}m"
+            elif self.display_mode == "s" and s > 0:
+                display_string = f"{time_sign}{s:.{self.precision}f}s"
+            else:
+                display_string = "―"
 
-        return f"{self.color['c']}―{self.color['R']}"
+        return self.color["c"] + display_string + self.color["R"]
 
     def _get_pretty_string(
         self,
         seconds: Union[int, float],
         display: Optional[bool] = False,
     ) -> str:
-        time_sign = "-" if seconds < 0 else ""
-        seconds = abs(round(seconds))
 
-        d = seconds // (3600 * 24)
-        h = seconds // 3600 % 24
-        m = seconds % 3600 // 60
-        s = seconds % 3600 % 60
+        if seconds == float("inf"):
+            pretty_string = "inf"
+        else:
+            time_sign = "-" if seconds < 0 else ""
+            seconds = abs(round(seconds))
 
-        if display or d > 0:
-            return f"{self.color['c']}{time_sign}{d:02d}d {h:02d}h {m:02d}m {s:02d}s{self.color['R']}"
-        elif h > 0:
-            return f"{self.color['c']}{time_sign}{h:02d}h {m:02d}m {s:02d}s{self.color['R']}"
-        elif m > 0:
-            return f"{self.color['c']}{time_sign}{m:02d}m {s:02d}s{self.color['R']}"
-        elif s > 0:
-            return f"{self.color['c']}{time_sign}{s:02d}s{self.color['R']}"
+            d = seconds // (3600 * 24)
+            h = seconds // 3600 % 24
+            m = seconds % 3600 // 60
+            s = seconds % 3600 % 60
 
-        return f"{self.color['c']}―{self.color['R']}"
+            if display or d > 0:
+                pretty_string = f"{time_sign}{d:02d}d {h:02d}h {m:02d}m {s:02d}s"
+            elif h > 0:
+                pretty_string = f"{time_sign}{h:02d}h {m:02d}m {s:02d}s"
+            elif m > 0:
+                pretty_string = f"{time_sign}{m:02d}m {s:02d}s"
+            elif s > 0:
+                pretty_string = f"{time_sign}{s:02d}s"
+            else:
+                pretty_string = "―"
+
+        return self.color["c"] + pretty_string + self.color["R"]
 
     def update(
         self,
@@ -134,7 +140,7 @@ class TimeQuota:
 
             if self.time_exceeded:
                 print(
-                    f"\n{self.color['g']}{self.name}{self.color['R']} {self.color['r']}⚠ TIME EXCEEDED!{self.color['R']}",
+                    f"{self.color['g']}{self.name} {self.color['r']}⚠ TIME EXCEEDED!{self.color['R']}",
                     f"Elapsed: {self._get_display_string(self.time_elapsed)}, Overflow: {self._get_display_string(abs(self.time_remaining))}",
                 )
             else:
@@ -168,31 +174,12 @@ class TimeQuota:
 
             if self.time_exceeded:
                 print(
-                    f"\n{self.color['g']}{self.name}{self.color['R']} {self.color['r']}⚠ TIME EXCEEDED!{self.color['R']}",
+                    f"{self.color['g']}{self.name} {self.color['r']}⚠ TIME EXCEEDED!{self.color['R']}",
                     f"Estimated: {self._get_display_string(self.time_elapsed + self.time_per_step)}",
                 )
 
         self.time_since = self.timer_fn()
         return self.time_exceeded
-
-    def range(
-        self,
-        *args: Any,
-        time_exceeded_fn: Optional[Callable] = None,
-        time_exceeded_break: Optional[bool] = True,
-        verbose: Optional[bool] = True,
-        **kwargs: Any,
-    ) -> Iterable[int]:
-        if not self.update(verbose=verbose):
-            for i in range(*args, **kwargs):
-                yield i
-
-                if self.track(verbose=verbose):
-                    if time_exceeded_fn is not None:
-                        time_exceeded_fn()
-
-                    if time_exceeded_break:
-                        break
 
     def iter(
         self,
@@ -213,11 +200,29 @@ class TimeQuota:
                     if time_exceeded_break:
                         break
 
+    def range(
+        self,
+        *args: Any,
+        time_exceeded_fn: Optional[Callable] = None,
+        time_exceeded_break: Optional[bool] = True,
+        verbose: Optional[bool] = True,
+        **kwargs: Any,
+    ) -> Iterable[int]:
+        return self.iter(
+            iterable=range(*args, **kwargs),
+            time_exceeded_fn=time_exceeded_fn,
+            time_exceeded_break=time_exceeded_break,
+            verbose=verbose,
+        )
+
     def reset(
         self,
     ) -> None:
         self.time_elapsed = 0
         self.time_remaining = self.quota
+
+        self.overflow = False
+        self.predicted_overflow = False
         self.time_exceeded = False
 
         self.time_steps = []
@@ -258,12 +263,12 @@ class TimeQuota:
             ],
             [
                 "Time Exceeded",
-                f"{self.color['r']}True{self.color['R']}"
-                if self.time_exceeded
-                else f"{self.color['c']}False{self.color['R']}",
-                f"{self.color['r']}True{self.color['R']}"
-                if self.time_exceeded
-                else f"{self.color['c']}False{self.color['R']}",
+                self.color["r" if self.time_exceeded else "c"]
+                + str(self.time_exceeded)
+                + self.color["R"],
+                self.color["r" if self.time_exceeded else "c"]
+                + str(self.time_exceeded)
+                + self.color["R"],
             ],
         ]
 
@@ -277,11 +282,7 @@ class TimeQuota:
     def __repr__(
         self,
     ) -> str:
-        _quota = self.quota
-        if self.mode == "m":
-            _quota /= 60
-        elif self.mode == "h":
-            _quota /= 3600
+        _quota = self.quota / _time_dict.get(self.mode)
 
         return (
             f"{self.__class__.__name__}"
